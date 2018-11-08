7b3760b83188758403f88e95814219bcb45b5ac8
acierate <33143065+acierate@users.noreply.github.com>
2018-11-08 16:31:38 -0600

    Update Language Basics, Variables Exercises/exercise 1.java

---
+++ Language Basics, Variables Exercises/exercise 1.java
@@ -0,0 +1,16 @@
+//Create a small program that defines some fields. Try creating some illegal field names and
+//see what kind of error the compiler produces. Use the naming rules and conventions as a guide
+package fields;
+
+public class Fields {
+public static void main(String[] args){
+	
+	char 2;
+	int 2notOkay = 2;
+	int okayUnderScore = 2_4;
+	double wrongDouble = 3._14;
+	
+	
+	
+}
+}
