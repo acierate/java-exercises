2a9c26935165f25398deb63b11c4a9c48d25e971
acierate <33143065+acierate@users.noreply.github.com>
2018-11-08 13:57:06 -0600

    Update Fan/fan.java

---
+++ Fan/fan.java
@@ -0,0 +1,15 @@
+package Fan;
+
+public class fan implements faninter {
+int speed = 0;
+
+public void changeSpeed(int newValue) {
+		speed = newValue;
+}
+
+void printStates() {
+	 System.out.println("speed:");
+}
+
+
+}
