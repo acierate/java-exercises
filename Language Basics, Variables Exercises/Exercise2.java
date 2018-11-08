4f2c1277339fd292350bb45e666ebcd3157bb735
acierate <33143065+acierate@users.noreply.github.com>
2018-11-08 16:31:33 -0600

    Update Language Basics, Variables Exercises/Exercise2.java

---
+++ Language Basics, Variables Exercises/Exercise2.java
@@ -0,0 +1,20 @@
+//In the program you created in Exercise 1, try leaving the fields uninitialized and print out their values.
+//Try the same with a local variable and see what kind of compiler errors you can produce.
+//Becoming familiar with common compiler errors will make it easier to recognize bugs in your code.
+package fields;
+
+public class Exercise2 {
+	public void main(String[] args){
+		
+		
+		int notOkay;
+		int okayUnderScore;
+		double wrongDouble;
+		
+		System.out.println(notOkay);
+		System.out.println(okayUnderScore);
+		System.out.println(wrongDouble);
+		
+		
+}
+}
